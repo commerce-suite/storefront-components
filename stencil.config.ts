d037ea5f3f30019ad2df397e85d31c8266c0bb08
Lucas Correa <lucasccorrea@gmail.com>
2023-09-29 17:04:38 -0300

    chore(estrutura-base): global.css adicionado com font-family default

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,7 @@ import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'front-components',
+  globalStyle: 'src/style/global.css',
   outputTargets: [
     {
       type: 'dist',
