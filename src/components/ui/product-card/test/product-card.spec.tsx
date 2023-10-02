a9014c66e30bc8e9a9641262071502862e222169
Lucas Correa <lucasccorrea@gmail.com>
2023-10-02 15:16:30 -0300

    feat(product): teste de variacoes adiciondo aos testes de product-card

--- src/components/ui/product-card/test/product-card.spec.tsx
+++ src/components/ui/product-card/test/product-card.spec.tsx
@@ -22,7 +22,7 @@ describe('product-card', () => {
     <product-card>
       <div class="product-container">
         <figure class="image">
-          <img alt="Imagem do produto" src="https://blog.myahaas.com.br/wp-content/uploads/2021/08/bolsas-que-combinam-com-tudo-como-comprar-900x900.jpg">
+          <img alt="Imagem do produto" src="${product.image.src}">
         </figure>
         <div class="info">
           <span class="title">
@@ -51,7 +51,7 @@ describe('product-card', () => {
     <product-card>
       <div class="-inline  product-container">
         <figure class="-inline image">
-          <img alt="Imagem do produto" src="https://blog.myahaas.com.br/wp-content/uploads/2021/08/bolsas-que-combinam-com-tudo-como-comprar-900x900.jpg">
+          <img alt="Imagem do produto" src="${product.image.src}">
         </figure>
         <div class="info">
           <span class="title">
@@ -81,7 +81,7 @@ describe('product-card', () => {
     <product-card>
       <div class="-inline  product-container">
         <figure class="-inline image">
-          <img alt="Imagem do produto" src="https://blog.myahaas.com.br/wp-content/uploads/2021/08/bolsas-que-combinam-com-tudo-como-comprar-900x900.jpg">
+          <img alt="Imagem do produto" src="${product.image.src}">
         </figure>
         <div class="info">
           <span class="title">
@@ -91,6 +91,68 @@ describe('product-card', () => {
             <span class="current">
               R$&nbsp;389,90
             </span>
+          </div>
+        </div>
+      </div>
+     </product-card>
+    `);
+  });
+
+  it('renders with variations', async () => {
+    const variations = [
+      {
+        label: 'Cor',
+        options: [
+          { name: 'branco', value: '12321' },
+          { name: 'azul', value: '12321' },
+        ],
+        currentValue: '12321',
+        modelKey: 'color_id',
+      },
+      {
+        label: 'Tamanho',
+        options: [
+          { name: 'M', value: '789' },
+          { name: 'P', value: '456' },
+          { name: 'G', value: '123' },
+        ],
+        currentValue: '456',
+        modelKey: 'lenght_id',
+      },
+    ];
+    const productWithVariations: IProductCard = {
+      ...product,
+      selectVariations: variations,
+    };
+    const page = await newSpecPage({
+      components: [ProductCard],
+      template: () => <product-card product={productWithVariations}></product-card>,
+    });
+    expect(page.root).toEqualHtml(`
+    <product-card>
+      <div class="product-container">
+        <figure class="image">
+          <img alt="Imagem do produto" src="${product.image.src}">
+        </figure>
+        <div class="info">
+          <span class="title">
+            Bolsa Essencial
+          </span>
+          <div class="price">
+            <span class="base">
+              R$&nbsp;499,99
+            </span>
+            <span class="current">
+              R$&nbsp;389,90
+            </span>
+          </div>
+        </div>
+        <div class="variations">
+          <div class="item">
+            <front-select label="Cor" value="12321"></front-select>
+          </div>
+          <div class="item">
+            <front-select label="Tamanho" value="456"></front-select>
           </div>
         </div>
       </div>
