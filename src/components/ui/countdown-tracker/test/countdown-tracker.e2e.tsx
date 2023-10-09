6d28bf767dcd016c01c3593acbef81731d497163
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-10-09 07:54:02 -0300

    test: (Componente Countdown) Refatora teste adicionando data de inicio do contador

--- src/components/ui/countdown-tracker/test/countdown-tracker.e2e.tsx
+++ src/components/ui/countdown-tracker/test/countdown-tracker.e2e.tsx
@@ -4,7 +4,7 @@ describe('countdown-tracker', () => {
   it('renders', async () => {
     const page = await newE2EPage();
     await page.setContent(
-      '<countdown-tracker  data-target-date="2024-11-06 13:24:00"></countdown-tracker>',
+      '<countdown-tracker data-initial-date="2024-11-05 13:24:00" data-target-date="2024-11-06 13:24:00"></countdown-tracker>',
     );
 
     const element = await page.find('countdown-tracker');
