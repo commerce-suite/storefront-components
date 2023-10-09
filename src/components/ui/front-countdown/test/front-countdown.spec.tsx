ef84c71b20df5a0193238d8822481e43a12dd265
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-10-09 15:14:34 -0300

    style: (Componente Countdown): Passa classes css para protocolo BEM

--- src/components/ui/front-countdown/test/front-countdown.spec.tsx
+++ src/components/ui/front-countdown/test/front-countdown.spec.tsx
@@ -10,26 +10,26 @@ describe('front-countdown', () => {
 
     expect(page.root).toEqualHtml(`
     <front-countdown data-initial-date="2024-11-05 13:24:00" data-target-date="2020-11-06 13:24:00">
-      <div class="front-countdown-header">
-        <h3 class="front-countdown-title">Agora falta muito pouco!</h3>
-        <p class="front-countdown-description">O produto que você tanto espera será liberado em breve.</p>
+      <div class="front-countdown__header">
+        <h3 class="front-countdown__title">Agora falta muito pouco!</h3>
+        <p class="front-countdown__description">O produto que você tanto espera será liberado em breve.</p>
       </div>
         <div class="front-countdown">
-          <div class="front-countdown-cell">
-            <p class="front-countdown-time">00</p>
-            <p class="front-countdown-date ">dias</p>
+          <div class="front-countdown__cell">
+            <p class="front-countdown__time">00</p>
+            <p class="front-countdown__unit ">dias</p>
           </div>
-          <div class="front-countdown-cell">
-            <p class="front-countdown-time">00</p>
-            <p class="front-countdown-date ">horas</p>
+          <div class="front-countdown__cell">
+            <p class="front-countdown__time">00</p>
+            <p class="front-countdown__unit ">horas</p>
           </div>
-          <div class="front-countdown-cell">
-            <p class="front-countdown-time">00</p>
-            <p class="front-countdown-date ">minutos</p>
+          <div class="front-countdown__cell">
+            <p class="front-countdown__time">00</p>
+            <p class="front-countdown__unit ">minutos</p>
           </div>
-          <div class="front-countdown-cell">
-            <p class="front-countdown-time">00</p>
-            <p class="front-countdown-date ">segundos</p>
+          <div class="front-countdown__cell">
+            <p class="front-countdown__time">00</p>
+            <p class="front-countdown__unit ">segundos</p>
           </div>
         </div>
     </front-countdown>
