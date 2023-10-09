a7c9e660e17938aa08773a5feb4f6b2f4dc42b57
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-10-09 07:55:18 -0300

    feat: (Componente Countdown) Refatora componente adicionando data inicial do contador

--- src/components/ui/countdown-tracker/ICountdownService.ts
+++ src/components/ui/countdown-tracker/ICountdownService.ts
@@ -1,6 +1,8 @@
 import { DateDifferenceType } from './countdown-tracker.type';
 
 export interface ICountdownService {
+  startCountdown(): void;
+  stopCountdown(): void;
   isCountdownFinished(): boolean;
   getMillisecondsDifference(): number;
   getTimeDifference(): DateDifferenceType;
