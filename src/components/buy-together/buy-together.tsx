c64337b2d21203cf9eb419aab3210c2ebf51337a
Lucas Correa <lucasccorrea@gmail.com>
2023-10-11 12:32:27 -0300

    feat(buy-together): inclusao do loading de componente

--- src/components/buy-together/buy-together.tsx
+++ src/components/buy-together/buy-together.tsx
@@ -27,17 +27,23 @@ export class BuyTogether implements ComponentWillLoad {
   @Event({ bubbles: true, eventName: 'on-buy-together-add-cart' })
   onBuyTogetherAddCartEvent: EventEmitter<IProductCard[]>;
 
+  @State() isLoading: boolean;
+
   @Method()
   async getBuyTogetherData() {
     return this.buyTogetherData;
   }
 
   public async load() {
+    this.isLoading = true;
     try {
       this.buyTogetherData = await this.buyTogetherService.getBuyTogetherByProductId(
         this.productId,
       );
-    } catch {}
+    } catch {
+    } finally {
+      this.isLoading = false;
+    }
   }
 
   private selectOrderBump(event: any, productOrderBumpId: number) {
@@ -110,51 +116,58 @@ export class BuyTogether implements ComponentWillLoad {
   }
 
   componentWillLoad(): void | Promise<void> {
-    return this.load();
+    this.load();
   }
 
   render() {
     return (
       <Host>
-        <form onSubmit={evt => this.onAddItemsToCart(evt)}>
-          <div class="title-wrapper">
-            <h2 class="title">{this.buyTogetherData.originalData.title || 'Compre Junto'}</h2>
+        {this.isLoading && (
+          <div class="loading-container">
+            <span class="spinner" />
           </div>
-          <section class="bagy-buy-together buy-together-container">
-            <div class="product-wrapper product-main">
-              <product-card
-                product={this.buyTogetherData.productMain}
-                onInputSelect={ev => this.onInputSelectProductMain(ev)}
-              ></product-card>
+        )}
+        {!this.isLoading && (
+          <form onSubmit={evt => this.onAddItemsToCart(evt)}>
+            <div class="title-wrapper">
+              <h2 class="title">{this.buyTogetherData.originalData.title || 'Compre Junto'}</h2>
             </div>
-            <div class="plus-icon">
-              <img src={getAssetPath('./assets/icons/icon-plus.svg')} alt="" />
-            </div>
-            <div class="products-order-bump">
-              {this.buyTogetherData.products.map(productCard => (
-                <div class="product-wrapper">
-                  <div class="checkbox-wrapper">
-                    <input
-                      type="checkbox"
-                      id={String(productCard.id)}
-                      onInput={ev => this.selectOrderBump(ev, productCard.id)}
-                    />
+            <section class="bagy-buy-together buy-together-container">
+              <div class="product-wrapper product-main">
+                <product-card
+                  product={this.buyTogetherData.productMain}
+                  onInputSelect={ev => this.onInputSelectProductMain(ev)}
+                ></product-card>
+              </div>
+              <div class="plus-icon">
+                <img src={getAssetPath('./assets/icons/icon-plus.svg')} alt="" />
+              </div>
+              <div class="products-order-bump">
+                {this.buyTogetherData.products.map(productCard => (
+                  <div class="product-wrapper">
+                    <div class="checkbox-wrapper">
+                      <input
+                        type="checkbox"
+                        id={String(productCard.id)}
+                        onInput={ev => this.selectOrderBump(ev, productCard.id)}
+                      />
+                    </div>
+                    <product-card
+                      inline
+                      product={productCard}
+                      onInputSelect={ev => this.onInputSelectOrderBump(ev)}
+                    ></product-card>
                   </div>
-                  <product-card
-                    inline
-                    product={productCard}
-                    onInputSelect={ev => this.onInputSelectOrderBump(ev)}
-                  ></product-card>
-                </div>
-              ))}
-            </div>
-            <div class="buy-btn-wrapper">
-              <button class="buy-btn" type="submit">
-                {this.buyTogetherData.originalData.buyButtonText || 'Comprar'}
-              </button>
-            </div>
-          </section>
-        </form>
+                ))}
+              </div>
+              <div class="buy-btn-wrapper">
+                <button class="buy-btn" type="submit">
+                  {this.buyTogetherData.originalData.buyButtonText || 'Comprar'}
+                </button>
+              </div>
+            </section>
+          </form>
+        )}
       </Host>
     );
   }
