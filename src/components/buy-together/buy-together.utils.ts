61f7a3494698071e2de9b19dc02f5647c4f20a85
Lucas Correa <lucasccorrea@gmail.com>
2023-10-26 15:54:04 -0300

    feat(launch-countdown): desabilita opcoes de atributos com lancamento ativo

---
+++ src/components/buy-together/buy-together.utils.ts
@@ -0,0 +1,13 @@
+import { Product } from '@uxshop/storefront-core/dist/modules/buy-together/BuyTogetherTypes';
+
+export const checkIsOutReleaseDate = (data?: Partial<Product>) => {
+  const { releaseDate: releaseDateObj } = data || {};
+  if (!releaseDateObj) return true;
+  const { now, releaseDate } = releaseDateObj;
+  return Number(now) >= Number(releaseDate);
+};
+
+export const checkHasBalance = (data?: Partial<Product>) => {
+  const { balance } = data || {};
+  return !!balance && balance > 0;
+};
