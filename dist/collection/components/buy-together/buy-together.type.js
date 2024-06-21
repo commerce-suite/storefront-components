09972e62bf30c7561ad7d0a6809f0678ad06b6f5
simaomeyrerdooca <simaomeyrer@gmail.com>
2024-06-21 18:33:04 -0300

    fix: remove dist

--- dist/collection/components/buy-together/buy-together.type.js
+++
@@ -1,6 +0,0 @@
-export var EnumBuyTogetherOnLoadStatus;
-(function (EnumBuyTogetherOnLoadStatus) {
-    EnumBuyTogetherOnLoadStatus["SHOULD_HIDDEN"] = "SHOULD_HIDDEN";
-    EnumBuyTogetherOnLoadStatus["SHOULD_SHOW"] = "SHOULD_SHOW";
-})(EnumBuyTogetherOnLoadStatus || (EnumBuyTogetherOnLoadStatus = {}));
-//# sourceMappingURL=buy-together.type.js.map
