af3d524d10d40e2b93fe09621075289e6202dff7
simaomeyrerdooca <simao.meyrer@bagy.com.br>
2024-07-03 11:27:23 -0300

    fix: remove dist

--- dist/components/front-select.js
+++
@@ -1,8 +0,0 @@
-import { F as FrontSelect$1, d as defineCustomElement$1 } from './front-select2.js';
-
-const FrontSelect = FrontSelect$1;
-const defineCustomElement = defineCustomElement$1;
-
-export { FrontSelect, defineCustomElement };
-
-//# sourceMappingURL=front-select.js.map
--- dist/types/components/buy-together/services/__mocks__/@uxshop/storefront-core/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { BuyTogether } from '@uxshop/storefront-core/dist/modules/buy-together/BuyTogetherTypes';
-export declare class BuyTogetherService {
-    static getByProductId(): Promise<BuyTogether>;
-}
