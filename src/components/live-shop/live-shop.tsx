7cc2ca91e1c5a6550c2be4c2b4a6d8aef7afc084
Fabrício Caldana Anelli <fabricio.anelli@bagy.com.br>
2024-08-21 10:00:53 -0300

    feat: separação de views

--- src/components/live-shop/live-shop.tsx
+++ src/components/live-shop/live-shop.tsx
@@ -48,91 +48,109 @@ export class LiveShop {
     this.componentRendered.emit();
   }
 
+  private renderLoading() {
+    return (
+      <div class="loading-container">
+        <span class="spinner" />
+      </div>
+    );
+  }
+
+  private renderWarmup() {
+    return (
+      <div class="live-shop-warmup">
+        <custom-card customClass="banner-custom-style" cardTitle={liveShopMock.title}>
+          {liveShopMock.banner ? (
+            <img src={liveShopMock.banner.src} alt={liveShopMock.banner.alt} />
+          ) : (
+            <div class="live-shop-banner" />
+          )}
+        </custom-card>
+      </div>
+    );
+  }
+
+  private renderInLiveMobile() {
+    return (
+      <div class="live-shop-in-live">
+        <div class="live-shop-in-live-player">
+          <live-video-player videoId={this.videoId} autoPlay />
+        </div>
+        <div class="live-shop-in-live-options">
+          <custom-card customClass="in-live-custom-style" cardTitle={liveShopMock.name}>
+            <tab-selector tabs={tabs(this.videoId, items)}></tab-selector>
+          </custom-card>
+        </div>
+      </div>
+    );
+  }
+
+  private renderInLiveDesktop() {
+    return (
+      <div class="live-shop-in-live-desktop">
+        <div class="live-shop-in-live-desktop-infos">
+          <div class="live-shop-in-live-desktop-infos-player">
+            <live-video-player videoId={this.videoId} autoPlay />
+          </div>
+          <div class="live-shop-in-live-desktop-infos-options">
+            <h2 class="live-shop-in-live-desktop-infos-options-title">{liveShopMock.name}</h2>
+            <button
+              class="live-shop-in-live-desktop-infos-options-button"
+              onClick={() => this.isChatOpenHandler()}
+            >
+              {this.buttonText()}
+            </button>
+          </div>
+          {this.isChatOpen && (
+            <div class="live-shop-in-live-desktop-infos-options-chat">
+              <live-video-chat videoId={this.videoId} />
+            </div>
+          )}
+        </div>
+        <div class="live-shop-in-live-desktop-content">
+          <div class="live-shop-in-live-desktop-content-card">
+            {items.length > 0 ? (
+              <highlight-card items={items}></highlight-card>
+            ) : (
+              <custom-card
+                customClass="in-live-custom-style-desktop"
+                cardTitle="produtos a caminho..."
+                cardDescription="Em breve, teremos algo especial para você!"
+              ></custom-card>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  private renderFinished() {
+    return (
+      <div class="live-shop-finished">
+        <custom-card
+          customClass="button-custom-style"
+          cardTitle="A live chegou ao fim!"
+          cardDescription="Fique de olho em nossas próximas lives para mais novidades e promoções imperdíveis!"
+        >
+          <button onClick={() => this.onReturnToHome.emit()}>Voltar para a página inicial</button>
+        </custom-card>
+      </div>
+    );
+  }
+
   render() {
+    if (this.isLoading) {
+      return <Host>{this.renderLoading()}</Host>;
+    }
+
     return (
       <Host>
-        {this.isLoading && (
-          <div class="loading-container">
-            <span class="spinner" />
-          </div>
-        )}
-        {!this.isLoading && (
-          <div class="live-shop">
-            {this.status === 'warmup' && (
-              <div class="live-shop-warmup">
-                <custom-card customClass="banner-custom-style" cardTitle={liveShopMock.title}>
-                  {liveShopMock.banner ? (
-                    <img src={liveShopMock.banner.src} alt={liveShopMock.banner.alt} />
-                  ) : (
-                    <div class="live-shop-banner" />
-                  )}
-                </custom-card>
-              </div>
-            )}
-            {this.status === 'inLive' && this.isSmallDevice ? (
-              <div class="live-shop-in-live">
-                <div class="live-shop-in-live-player">
-                  <live-video-player videoId={this.videoId} autoPlay />
-                </div>
-                <div class="live-shop-in-live-options">
-                  <custom-card customClass="in-live-custom-style" cardTitle={liveShopMock.name}>
-                    <tab-selector tabs={tabs(this.videoId, items)}></tab-selector>
-                  </custom-card>
-                </div>
-              </div>
-            ) : (
-              <div class="live-shop-in-live-desktop">
-                <div class="live-shop-in-live-desktop-infos">
-                  <div class="live-shop-in-live-desktop-infos-player">
-                    <live-video-player videoId={this.videoId} autoPlay />
-                  </div>
-                  <div class="live-shop-in-live-desktop-infos-options">
-                    <h2 class="live-shop-in-live-desktop-infos-options-title">
-                      {liveShopMock.name}
-                    </h2>
-                    <button
-                      class="live-shop-in-live-desktop-infos-options-button"
-                      onClick={() => this.isChatOpenHandler()}
-                    >
-                      {this.buttonText()}
-                    </button>
-                  </div>
-                  {this.isChatOpen && (
-                    <div class="live-shop-in-live-desktop-infos-options-chat">
-                      <live-video-chat videoId={this.videoId} />
-                    </div>
-                  )}
-                </div>
-                <div class="live-shop-in-live-desktop-content">
-                  <div class="live-shop-in-live-desktop-content-card">
-                    {items.length > 0 ? (
-                      <highlight-card items={items}></highlight-card>
-                    ) : (
-                      <custom-card
-                        customClass="in-live-custom-style-desktop"
-                        cardTitle="produtos a caminho..."
-                        cardDescription="Em breve, teremos algo especial para você!"
-                      ></custom-card>
-                    )}
-                  </div>
-                </div>
-              </div>
-            )}
-            {this.status === 'finished' && (
-              <div class="live-shop-finished">
-                <custom-card
-                  customClass="button-custom-style"
-                  cardTitle="A live chegou ao fim!"
-                  cardDescription="Fique de olho em nossas próximas lives para mais novidades e promoções imperdíveis!"
-                >
-                  <button onClick={() => this.onReturnToHome.emit()}>
-                    Voltar para a página inicial
-                  </button>
-                </custom-card>
-              </div>
-            )}
-          </div>
-        )}
+        <div class="live-shop">
+          {this.status === 'warmup' && this.renderWarmup()}
+          {this.status === 'inLive' &&
+            (this.isSmallDevice ? this.renderInLiveMobile() : this.renderInLiveDesktop())}
+          {this.status === 'finished' && this.renderFinished()}
+        </div>
       </Host>
     );
   }
