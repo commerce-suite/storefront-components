7bff5b87dc3aa89846122053806da391c7a8d9a9
simaomeyrerdooca <simao.meyrer@bagy.com.br>
2025-02-07 17:56:04 -0300

    fix: remove dist

--- dist/collection/components/ui/front-select/front-select.type.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=front-select.type.js.map
--- dist/components/front-image.js
+++
@@ -1,8 +0,0 @@
-import { F as FrontImage$1, d as defineCustomElement$1 } from './front-image2.js';
-
-const FrontImage = FrontImage$1;
-const defineCustomElement = defineCustomElement$1;
-
-export { FrontImage, defineCustomElement };
-
-//# sourceMappingURL=front-image.js.map
