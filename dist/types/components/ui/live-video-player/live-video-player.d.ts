7c25e13f0b8ad1829aa93f6531806726ee59a52f
simaomeyrerdooca <simao.meyrer@bagy.com.br>
2025-02-11 13:41:57 -0300

    fix: mini player mute prop

--- dist/types/components/ui/live-video-player/live-video-player.d.ts
+++ dist/types/components/ui/live-video-player/live-video-player.d.ts
@@ -2,6 +2,7 @@ import { EventEmitter } from '../../../stencil-public-runtime';
 export declare class LiveVideoPlayer {
     videoId: string;
     autoPlay: boolean;
+    mute: boolean;
     componentRendered: EventEmitter<void>;
     private getVideoUrl;
     componentDidLoad(): void;
