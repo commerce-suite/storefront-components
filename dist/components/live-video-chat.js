10f4a33f7ffa8a5579d7b594a6a0583d95a3fe13
Fabrício Caldana Anelli <fabricio.anelli@bagy.com.br>
2025-08-18 09:41:32 -0300

    chore: update dist

--- dist/components/live-video-chat.js
+++
@@ -1,8 +0,0 @@
-import { L as LiveVideoChat$1, d as defineCustomElement$1 } from './live-video-chat2.js';
-
-const LiveVideoChat = LiveVideoChat$1;
-const defineCustomElement = defineCustomElement$1;
-
-export { LiveVideoChat, defineCustomElement };
-
-//# sourceMappingURL=live-video-chat.js.map
--- dist/types/components/ui/live-video-chat/live-video-chat.d.ts
+++
@@ -1,8 +0,0 @@
-import { EventEmitter } from '../../../stencil-public-runtime';
-export declare class LiveVideoChat {
-    videoId: string;
-    componentRendered: EventEmitter<void>;
-    private getChatUrl;
-    componentDidLoad(): void;
-    render(): any;
-}
